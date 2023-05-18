5003c4b6b2b472c1e5adf5befcc6de34eb269a59
SpaceCatKun <dontclosespacecat@gmail.com>
2023-05-18 17:07:54 +0900

    0518

--- GP_0516/Assets/script/word/Draggables.cs
+++ GP_0516/Assets/script/word/Draggables.cs
@@ -7,15 +7,20 @@ public class Draggables : MonoBehaviour
     public bool IsDragging;
     public string word = "A";
     public Vector3 LastPosition;
+    public Vector3 checkp;
 
     private Collider2D _collider;
     private DragController _dragController;
     private WordMKController _wordMKcontroller;
-    private float _movemonetTime = 15f;
+    private Rigidbody2D _rigidbody;
+    private float _movemonetTime = 7f;
     private System.Nullable<Vector3> _movemonetDestination;
+    public int wordindex = 0;
+    public bool checkb;
 
     void Start()
     {
+        _rigidbody = GetComponent<Rigidbody2D>();
         _collider = GetComponent<Collider2D>();
         _dragController = FindObjectOfType<DragController>();
         _wordMKcontroller = FindObjectOfType<WordMKController>();
@@ -24,9 +29,8 @@ public class Draggables : MonoBehaviour
     {
         if(_movemonetDestination.HasValue)
         {
-            if(IsDragging)
+            if (IsDragging)
             {
-                _wordMKcontroller.word_1 = null;
                 _movemonetDestination = null;
                 return;
             }
@@ -38,30 +42,89 @@ public class Draggables : MonoBehaviour
             }
             else
             {
-                _wordMKcontroller.word_1 = word;
-                transform.position = Vector3.Lerp(transform.position, _movemonetDestination.Value, _movemonetTime * Time.fixedDeltaTime);
+                _rigidbody.gravityScale = 0f;
+                if (_rigidbody.gravityScale == 0f)
+                {
+                    transform.position = Vector3.Lerp(transform.position, _movemonetDestination.Value, _movemonetTime * Time.fixedDeltaTime);
+                    checkp = _movemonetDestination.Value;
+                }
             }
         }
+        checkb = true;
     }
 
     void OnTriggerEnter2D(Collider2D other)
     {
         Draggables collidedDraggable = other.GetComponent<Draggables>();
 
-        if(collidedDraggable != null && _dragController.LastDragged.gameObject == gameObject)
+        if (collidedDraggable != null && _dragController.LastDragged.gameObject == gameObject)
         {
             ColliderDistance2D colliderDistance2D = other.Distance(_collider);
             Vector3 diff = new Vector3(colliderDistance2D.normal.x, colliderDistance2D.normal.y) * colliderDistance2D.distance;
             transform.position -= diff;
         }
 
-        if(other.CompareTag("Dragin"))
+        if(other.CompareTag("Dragin1"))
+        {
+            checkp = new Vector3(18.5f, 3.5f, 7.955483f);
+            _movemonetDestination = other.transform.position;
+            _wordMKcontroller.word_1 = word;
+            _collider.enabled = false;
+        }
+        else if (other.CompareTag("Dragin2"))
         {
             _movemonetDestination = other.transform.position;
+            _wordMKcontroller.word_2 = word;
+            _collider.enabled = false;
+        }
+        else if (other.CompareTag("Dragin3"))
+        {
+            _movemonetDestination = other.transform.position;
+            _wordMKcontroller.word_3 = word;
+            _collider.enabled = false;
+        }
+        else if (other.CompareTag("Dragin4"))
+        {
+            _movemonetDestination = other.transform.position;
+            _wordMKcontroller.word_4 = word;
+            _collider.enabled = false;
+        }
+        else if (other.CompareTag("Dragin5"))
+        {
+            _movemonetDestination = other.transform.position;
+            _wordMKcontroller.word_5 = word;
+            _collider.enabled = false;
         }
         else if (other.CompareTag("Dragnot"))
         {
             _movemonetDestination = LastPosition;
+        }
+    }
+    private void removeword(int index)
+    {
+        if(index == 1)
+        {
+            _wordMKcontroller.word_1 = null;
+        }
+        else if(index == 2)
+        {
+            _wordMKcontroller.word_2 = null;
+        }
+        else if (index == 3)
+        {
+            _wordMKcontroller.word_3 = null;
+        }
+        else if (index == 4)
+        {
+            _wordMKcontroller.word_4 = null;
+        }
+        else if (index == 5)
+        {
+            _wordMKcontroller.word_5 = null;
+        }
+        else
+        {
+            return;
         }
     }
 }
--- GP_0516/Assets/script/word/WordMKController.cs
+++ GP_0516/Assets/script/word/WordMKController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System;
 
 public class WordMKController : MonoBehaviour
 {
@@ -13,10 +14,29 @@ public class WordMKController : MonoBehaviour
     public string word_4;
     public string word_5;
     private string mkingword;
+    private string[] c_word = { "ABCDE" };
+    private string index;
+    private GameObject tower;
+    private string path = "M_word/";
 
-    void Update()
+    void FixedUpdate()
     {
         mkingword = word_1 + word_2 + word_3 + word_4 + word_5;
         MKword.text = mkingword;
+    }
+
+    public void MKbutton()
+    {
+        index = Array.Find(c_word, element => element == mkingword);
+        if(index != null)
+        {
+            path = path + mkingword;
+            tower = Resources.Load<GameObject>(path);
+            Instantiate(tower);
+        }
+        else
+        {
+            Debug.Log("not");
+        }
     }
 }
